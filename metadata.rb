aebc73364aee526c00ff983fa65945aaa46e141d
Nick Weaver <jnweaver@wisc.edu>
2014-12-29 13:47:40 -0600

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jnweaver@wisc.edu'
 license          'All rights reserved'
 description      'Installs/Configures umark_ruby'
 long_description 'Installs/Configures umark_ruby'
-version          '0.1.5'
+version          '0.1.6'
 
 depends "selinux", "= 0.8.0"
 depends "build-essential", "~> 2.0.6"
