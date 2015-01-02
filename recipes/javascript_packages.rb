d121292fa1542acb3d2e748ae8553849e252c5fc
Nick Weaver <jnweaver@wisc.edu>
2015-01-02 14:23:01 -0600

    Use tmp_npm shell script to run npm i without NFS permission issues

--- recipes/javascript_packages.rb
+++ recipes/javascript_packages.rb
@@ -10,7 +10,15 @@
 include_recipe "nodejs"
 include_recipe "grunt_cookbook::install_grunt_cli"
 
-grunt_cookbook_npm node['umark_ruby']['project_root'] do
-  action :install
-  flags "--no-bin-link --unsafe-perm"
+cookbook_file "tmp_npm.sh" do
+  path node['umark_ruby']['project_root'] + "tmp_npm.sh"
+  mode '0755'
+  action :create
+end
+
+execute "run tmp npm install to bypass NFS permission issues" do
+  cwd node['umark_ruby']['project_root']
+  # user "vagrant"
+  action :run
+  command "./tmp_npm.sh install"
 end
