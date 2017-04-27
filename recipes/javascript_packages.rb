e9657b96f98c7fa84ca2a3d5dd5c151bd829a3be
Nick Weaver <jnweaver@wisc.edu>
2017-04-27 08:03:28 -0500

    Remove grunt cookbook; install grunt-cli via node_npm

--- recipes/javascript_packages.rb
+++ recipes/javascript_packages.rb
@@ -8,17 +8,19 @@
 #
 
 include_recipe "nodejs"
-include_recipe "grunt_cookbook::install_grunt_cli"
+# include_recipe "grunt_cookbook::install_grunt_cli"
 
-cookbook_file "tmp_npm.sh" do
-  path node['umark_ruby']['project_root'] + "tmp_npm.sh"
-  mode '0755'
-  action :create
-end
+nodejs_npm "grunt-cli"
 
-execute "run tmp npm install to bypass NFS permission issues" do
-  cwd node['umark_ruby']['project_root']
-  # user "vagrant"
-  action :run
-  command "./tmp_npm.sh install"
-end
+# cookbook_file "tmp_npm.sh" do
+#   path node['umark_ruby']['project_root'] + "tmp_npm.sh"
+#   mode '0755'
+#   action :create
+# end
+
+# execute "run tmp npm install to bypass NFS permission issues" do
+#   cwd node['umark_ruby']['project_root']
+#   # user "vagrant"
+#   action :run
+#   command "./tmp_npm.sh install"
+# end
