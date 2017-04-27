85850c4b16047e824482eb07439d9207a4c31526
Nick Weaver <jnweaver@wisc.edu>
2017-04-27 08:28:23 -0500

    Revert to shell npm install workaround

--- recipes/javascript_packages.rb
+++ recipes/javascript_packages.rb
@@ -10,22 +10,24 @@
 include_recipe "nodejs"
 # include_recipe "grunt_cookbook::install_grunt_cli"
 
-nodejs_npm 'app' do
-  path node['umark_ruby']['project_root']
-  json true
+# nodejs_npm 'app' do
+#   path node['umark_ruby']['project_root']
+#   json true
+# end
+
+
+
+cookbook_file "tmp_npm.sh" do
+  path node['umark_ruby']['project_root'] + "tmp_npm.sh"
+  mode '0755'
+  action :create
+end
+
+execute "run tmp npm install to bypass NFS permission issues" do
+  cwd node['umark_ruby']['project_root']
+  # user "vagrant"
+  action :run
+  command "./tmp_npm.sh install"
 end
 
 nodejs_npm "grunt-cli"
-
-# cookbook_file "tmp_npm.sh" do
-#   path node['umark_ruby']['project_root'] + "tmp_npm.sh"
-#   mode '0755'
-#   action :create
-# end
-
-# execute "run tmp npm install to bypass NFS permission issues" do
-#   cwd node['umark_ruby']['project_root']
-#   # user "vagrant"
-#   action :run
-#   command "./tmp_npm.sh install"
-# end
